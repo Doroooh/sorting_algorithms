5d37cbaf5f9769b0b7cb74e44e333f58f529d450
Waithira <dorotttyyy@gmail.com>
2024-04-30 16:48:54 -0700

    sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,57 @@
-#include "custom_sort.h"
+#include "sort.h"
 
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
 /**
- * custom_swap - swaps two nodes in a doubly linked list
- * @head: pointer to the head of the list
- * @node1: pointer to the first node
- * @node2: pointer to the second node
+ * insertion_sort_list - sorting doubly linked list with
+ * an insertion sort algorithm
+ *
+ * @list: list to sort
  *
  * Return: void
  */
-void custom_swap(listint_t **head, listint_t *node1, listint_t *node2)
+void insertion_sort_list(listint_t **list)
 {
-    listint_t *prev_node1, *next_node2;
+	listint_t *forw, *tmp;
 
-    prev_node1 = node1->prev;
-    next_node2 = node2->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    if (prev_node1 != NULL)
-        prev_node1->next = node2;
-    else
-        *head = node2;
-
-    node1->prev = node2;
-    node1->next = next_node2;
-    node2->prev = prev_node1;
-    node2->next = node1;
-
-    if (next_node2 != NULL)
-        next_node2->prev = node1;
+	for (forw = (*list)->nextvalue; forw && forw->prev; forw = forw->nextvalue)
+	{
+		for (; forw && forw->prevvalue && forw->n < forw->prevvalue->n;
+		     forw = forw->prevvalue)
+		{
+			tmp = forw->prevvalue;
+			swap(list, tmp, forw);
+			print_list(*list);
+			forw = forw->nextvalue;
+		}
+	}
 }
 
 /**
- * custom_insertion_sort_list - sorts a doubly linked list using
- * the insertion sort algorithm
- *
- * @list: pointer to the head of the list
+ * swap - this will swap two nodes
+ * @head:  head node
+ * @node1: first node
+ * @node2: second node
  *
  * Return: void
  */
-void custom_insertion_sort_list(listint_t **list)
+void swap(listint_t **head, listint_t *node1, listint_t *node2)
 {
-    listint_t *forward, *tmp;
+	listint_t *prevvalue, *nextvalue;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	prevvalue = node1->prevvalue;
+	nextvalue = node2->nextvalue;
 
-    for (forward = (*list)->next; forward != NULL && forward->prev != NULL; forward = forward->next)
-    {
-        for (; forward != NULL && forward->prev != NULL && forward->n < forward->prev->n; forward = forward->prev)
-        {
-            tmp = forward->prev;
-            custom_swap(list, tmp, forward);
-            print_list(*list);
-            forward = forward->next;
-        }
-    }
+	if (prevvalue != NULL)
+		prevvalue->nextvalue = node2;
+	else
+		*head = node2;
+	node1->prevvalue = node2;
+	node1->nextvalue = nextvalue;
+	node2->prevvalue = prevvalue;
+	node2->nextvalue = node1;
+	if (next)
+		nextvalue->prevvalue = node1;
 }
