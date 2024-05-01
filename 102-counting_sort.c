6910c9695a359e40a23cf9e9b9a52bd49f6cbed6
Waithira <dorotttyyy@gmail.com>
2024-04-30 17:27:01 -0700

    sorting algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 
 /**
- * counting_sort - sorts an array of integers in ascending order using a
+ * counting_sort - sorting array of integers in ascending order using the
  * counting sort algorithm
- * @array: array of integers to be sorted
+ * @array: array of integers to  sort
  * @size: amount of elements in array
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, total, max;
+	int k, total, max;
 	int *count, *output;
 
 	if (!array || size < 2)
 		return;
 
 	max = array[0];
-	for (i = 0; i < (int)size; i++)
+	for (k = 0; k < (int)size; k++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		if (array[k] > max)
+			max = array[k];
 	}
 
 	count = calloc((max + 1), sizeof(int));
 	if (!count)
 		return;
-	for (i = 0; i < (int)size; i++)
-		count[array[i]]++;
+	for (k = 0; k < (int)size; k++)
+		count[array[k]]++;
 
-	for (i = 0, total = 0; i < max + 1; i++)
+	for (k = 0, total = 0; k < max + 1; k++)
 	{
-		total = count[i] + total;
-		count[i] = total;
+		total = count[k] + total;
+		count[k] = total;
 	}
 	print_array(count, max + 1);
 
@@ -39,14 +39,14 @@ void counting_sort(int *array, size_t size)
 	if (!output)
 		return;
 
-	for (i = 0; i < (int)size; i++)
+	for (k = 0; k < (int)size; k++)
 	{
-		output[count[array[i]] - 1] = array[i];
-		count[array[i]]--; /* needed to handle identical values */
+		output[count[array[k]] - 1] = array[k];
+		count[array[k]]--; /*handling the identical values*/
 	}
 
-	for (i = 0; i < (int)size; i++)
-		array[i] = output[i];
+	for (k = 0; k < (int)size; k++)
+		array[k] = output[k];
 	free(count);
 	free(output);
 }
